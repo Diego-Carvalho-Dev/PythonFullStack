fa05002a6ff4a52e37f3e5842e5b47a6e117fbe9
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-11 05:15:05 -0300

    operadores relacionais

---
+++ Intordução_Python/6_operadores_relacional.py
@@ -0,0 +1,10 @@
+ # > < == != <= =>
+
+
+operador_maior = 2 > 1 # true
+operador_menor = 1 < 2 # true
+operador_igual = 2 == 2 # true
+operador_diferente = 2 != 1 # true
+operador_maior_e_igual = 2 >= 2 # true
+operador_menor_e_igual = 2 <= 1 # false
+

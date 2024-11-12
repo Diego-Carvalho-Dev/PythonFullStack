f8fbb3ccd0bc52089cc5f67f8bc60bb8445f9638
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-12 04:33:16 -0300

    exercicio positivo e negativo

--- Intordução_Python/11_exercicio_positivo_netivo.py
+++ Intordução_Python/11_exercicio_positivo_netivo.py
@@ -1,2 +1,10 @@
 #Receba um número e exiba se ele é positivo ou negativo.
 
+numero = int(input('Digite o número: '))
+
+if numero > 0:
+    print(f'O numero {numero} é positivo')
+elif numero < 0:
+    print(f'O numero {numero} é negativo')
+else:
+    print(f'O número {numero} é zero')

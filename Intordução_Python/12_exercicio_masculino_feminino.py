efebbd748eea52fb93cadcbc84da16571a0b9c2d
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-12 05:00:51 -0300

    exercios masculino e feminino

---
+++ Intordução_Python/12_exercicio_masculino_feminino.py
@@ -0,0 +1,12 @@
+#receba M para masculino e F par feminino exiba o sexo da pessoa·
+
+sexo = input('Digite F par feminino e M para masculino: ')
+
+
+if sexo == 'F' or sexo == 'f':
+    print('o sexo é de feminino')
+elif sexo == 'M' or sexo == 'm':
+    print('o sexo é de Masculino')
+else:
+     print('Valor inválido')
+

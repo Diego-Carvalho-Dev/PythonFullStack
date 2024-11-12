e64182becfa6c8748b527b0291fb4e1f16ee914d
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-12 05:53:00 -0300

    extruturaa de repeticao

--- Intordução_Python/14_extrutura_de_repeticao_white.py
+++ Intordução_Python/14_extrutura_de_repeticao_white.py
@@ -1,9 +1,21 @@
 
 i = 0
+j = 0
 
-#enquanto o i < do que 5 ele vai printar.
-
-while i < 5:
+while i < 10 and j < 30:
     print('Olá tudo bem ?')
+    break
+    i = i + 1
+    j = j + 10
+
+#---------------------------------------
+
+i = 0
+
+while i < 10:
+    print(i)
+    if i % 2 == 1:
+        i = i + 2
+        continue
     i = i + 1
 

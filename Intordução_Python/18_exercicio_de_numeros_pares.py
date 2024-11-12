f4a759c8761e91a42031930d1351ed8dc1c38c4b
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-12 08:53:50 -0300

    exercicio de numeros

---
+++ Intordução_Python/18_exercicio_de_numeros_pares.py
@@ -0,0 +1,10 @@
+#Receba um número e mostre todos os números pares de 0 até o número digitado.
+
+numero = int(input('Digite um numero: '))
+
+i = 0
+while i <= numero:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+

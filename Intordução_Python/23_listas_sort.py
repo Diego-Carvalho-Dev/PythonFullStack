640592847b2a795834b755a900d94022a1297c5a
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-13 08:36:59 -0300

    listas com sort, sorted, reverse

---
+++ Intordução_Python/23_listas_sort.py
@@ -0,0 +1,11 @@
+
+#SORT ORDENA A LISTA....
+#REVERSE, ORDENA AO CONTRARIO
+#SORTED CRIA UMA NOVA LISTA
+
+nomes = ['Diego', 'Michele', 'Derick']
+nomes2 = ['Diego', 'Michele', 'Derick']
+nomes.sort(reverse=True)
+nomes_ordenado = sorted(nomes2, reverse=True)
+
+print(nomes,nomes2)

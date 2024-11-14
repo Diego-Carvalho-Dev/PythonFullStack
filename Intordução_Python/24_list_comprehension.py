4f84f576d6827f0170fc3c4aeed2194cede775a4
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-14 07:54:07 -0300

    list comprehension

---
+++ Intordução_Python/24_list_comprehension.py
@@ -0,0 +1,29 @@
+
+# z = [i*2 for i in range(0, 10)]
+# print(z)
+
+# #####################################################
+
+# y = []
+# for index in range(0,10):
+#     y.append(index)
+
+# print(y)
+
+# #####################################################
+
+# m = [ [j for j in range(4,7)] for i in range(0, 10)]
+# print(m)
+
+# #####################################################
+
+# p = [ input('Digite um numero: ') for i in range(0, 1)]
+# print(p)
+
+# #####################################################
+
+# v = [ [ [d for d in range(0,10)] for s in range(0, 10)] for t in range(0, 10)]
+# print(v)
+
+u = [t for t in range(0, 10) if t > 4]
+print(u)

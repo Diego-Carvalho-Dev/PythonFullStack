d3f45400fabc1d3d8a38f8d296c3ff8f6e6288df
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-14 07:18:28 -0300

    lista e soma

---
+++ Intordução_Python/23_listas_soma.py
@@ -0,0 +1,11 @@
+#Receba 10 valores e exiba a soma de todos eles
+
+array = [1,3,5,8,5,6,7,8,9,10]
+print(sum(array))
+
+
+##################################################
+
+numeros = [int(input('numero')) for i in range(0,10)]
+
+print(sum(array))

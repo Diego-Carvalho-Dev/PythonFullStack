afedd92a366322622189ab4ab472d1fb5c3a2c13
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-11-21 08:56:38 -0300

    Introducao a POO

---
+++ Introdução_POO/exemplo.py
@@ -0,0 +1,30 @@
+#EXTRUTURAL 
+
+x = []
+
+for i in range(0,10):
+    if i % 2 == 0:
+        x.append(i)
+
+print(x)
+
+########################################################
+
+#LIST COMPREENSION
+
+y = [i for i in range(0, 10) if i % 2 == 0]
+
+
+########################################################
+
+# POO = MODELAGEM DE DADOS(ENTIDADE)
+#PESSOA = ENTIDADE 
+#IIDADE, RG, CPF = ATRIBUTOS
+#LOGAR, CADASTRAR..
+
+#ENTIDADE VAI TER: ATRIBUTOS E MÉTODOS
+
+
+
+
+
